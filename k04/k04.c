3a25ed0ddc7aaca430a0822e060a35c3ea6ac26e
Miyajima <riku3840@outlook.jp>
2021-01-08 19:02:49 +0900

    0108

--- k04/k04.c
+++ k04/k04.c
@@ -110,7 +110,7 @@ void QuickSort(City arrayCity[], int left, int right)
      pivot=arrayCity[left].seafood;
      while (1)
      {
-         while(arrayCity[i].seafood<=pivot && i<=right)
+         while(arrayCity[i].seafood>pivot && i<=right)
          {
           i++;
          }
@@ -118,8 +118,11 @@ void QuickSort(City arrayCity[], int left, int right)
          {
           j--;
          }
-         if(i>=j)
+         if(i=j)
          {
+             a=arrayCity[left];
+             arrayCity[left]=arrayCity[j];
+             arrayCity[j]=a;
              break;
          }
          else
